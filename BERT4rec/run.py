6166a5f47e98983639544abed97deecdc35173f6
Sasha Petrov <firexel@gmail.com>
2021-11-04 06:34:33 +0000

    removed experimental code

--- BERT4rec/run.py
+++ BERT4rec/run.py
@@ -612,6 +612,4 @@ def main(_):
 
 
 if __name__ == "__main__":
-    physical_devices = tf.config.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
     tf.compat.v1.app.run()
